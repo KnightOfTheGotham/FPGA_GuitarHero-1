eeb48d66e4dfb0ba24f3e7066d11edca54ce674d
Selin Fildiş <selinfildis@gmail.com>
2016-05-02 12:00:07 +0300

    Update controlUnit.sv

--- new/controlUnit.sv
+++ new/controlUnit.sv
@@ -1,5 +1,7 @@
 module controlUnit(input logic clk, buttonIn, output logic [6:0] pos);
 	logic countersig;
-	controllerFSM(buttonIn, countersig);
+	logic timer;
+	//timer için clk divider lazım o eklencek
+	controllerFSM(clk,buttonIn,timer, countersig);
 	controllerCounter(countersig,pos);
 endmodule
