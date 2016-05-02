d23e2757b83f0d425ea6874de851f7cbf8d85bc1
Selin Fildiş <selinfildis@gmail.com>
2016-05-02 11:38:35 +0300

    Update pointKeeper.sv

--- new/pointKeeper.sv
+++ new/pointKeeper.sv
@@ -1,8 +1,8 @@
-module pointKeeper(input logic comp, output reg [7:0] pointOut, reg overflow);
-	reg[7:0] points;
-	initial begin points=8'b0;end
+module pointKeeper(input logic comp, output reg [6:0] pointOut, reg overflow);
+	reg[6:0] points;
+	initial begin points=7'b0;end
 	always_comb
-		if(points<=8'b11111111)
+		if(points<=7'b11111111)
 			if(comp)
 				points=points+1;
 			else 
