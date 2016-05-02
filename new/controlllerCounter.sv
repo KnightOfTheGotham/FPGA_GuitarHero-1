3d74cf3736324b29a4e95c5877e22c361d20b2a6
Selin Fildiş <selinfildis@gmail.com>
2016-05-02 10:30:09 +0300

    Update controlllerCounter.sv

--- new/controlllerCounter.sv
+++ new/controlllerCounter.sv
@@ -1,5 +1,8 @@
 module controllerCounter(input logic sig, output log[6:0] pos);
 	logic [6:0] posCount;
+	initial begin posCount=7'b0; end
+	assign pos=posCount;
 	always@(posedge sig)
 		posCount<=posCount+1;
+	
 endmodule
