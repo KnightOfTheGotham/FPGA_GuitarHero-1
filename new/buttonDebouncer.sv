6ac1ba9b35438cf8677203fedd8e386cde4e51f9
Selin Fildiş <selinfildis@gmail.com>
2016-05-03 14:54:22 +0300

    Update buttonDebouncer.sv

--- new/buttonDebouncer.sv
+++ new/buttonDebouncer.sv
@@ -1,31 +1,27 @@
 module PushButton_Debouncer(
-    input logic clk,
-    input logic PB,  // "PB" is the glitchy, asynchronous to clk, active low push-button signal
-
-    // from which we make three outputs, all synchronous to the clock
-    output logic PB_state  // 1 as long as the push-button is active (down)
-    
+    input clk, //this is a 50MHz clock provided on FPGA pin PIN_Y2
+    input PB,  //this is the input to be debounced
+     output reg PB_state  //this is the debounced switch
 );
-// First use two flip-flops to synchronize the PB signal the "clk" clock domain
-reg PB_sync_0;  always @(posedge clk) PB_sync_0 <= ~PB;  // invert PB to make PB_sync_0 active high
-reg PB_sync_1;  always @(posedge clk) PB_sync_1 <= PB_sync_0;
+/*This module debounces the pushbutton PB.
+ *It can be added to your project files and called as is:
+ *DO NOT EDIT THIS MODULE
+ */
 
-// Next declare a 16-bits counter
-logic [15:0] PB_cnt;
+// Synchronize the switch input to the clock
+reg PB_sync_0;
+always @(posedge clk) PB_sync_0 <= PB; 
+reg PB_sync_1;
+always @(posedge clk) PB_sync_1 <= PB_sync_0;
 
-// When the push-button is pushed or released, we increment the counter
-// The counter has to be maxed out before we decide that the push-button state has changed
-
-logic PB_idle = (PB_state==PB_sync_1);
-logic PB_cnt_max = &PB_cnt;	// true when all bits of PB_cnt are 1's
-
+// Debounce the switch
+reg [15:0] PB_cnt;
 always @(posedge clk)
-if(PB_idle)
-    PB_cnt <= 0;  // nothing's going on
+if(PB_state==PB_sync_1)
+    PB_cnt <= 0;
 else
 begin
-    PB_cnt <= PB_cnt + 16'd1;  // something's going on, increment the counter
-    if(PB_cnt_max) PB_state <= ~PB_state;  // if the counter is maxed out, PB changed!
+    PB_cnt <= PB_cnt + 1'b1;  
+    if(PB_cnt == 16'hffff) PB_state <= ~PB_state;  
 end
-
 endmodule
