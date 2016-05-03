49f248898bf591adcc5b41e2dedfa6fc976bb84a
Selin Fildiş <selinfildis@gmail.com>
2016-05-03 14:54:03 +0300

    Update pointKeeper.sv

--- new/pointKeeper.sv
+++ new/pointKeeper.sv
@@ -1,10 +1,9 @@
-module pointKeeper(input logic comp, output logic [6:0] pointOut);
-	logic[6:0] points;
-	initial begin points=7'b0;end
-	assign pointOut = points;
+module pointKeeper(input logic clk, comp, output reg [7:0] pointOut);
+	reg[7:0] points;
+	initial begin points=8'b0;end
 	always@(posedge comp)
-		if(points<7'b1111111)
+			
 				points=points+1;
-		else 
-			begin points = 7'b0; end
+			
+	assign pointOut=points;	
 endmodule
